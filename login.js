bd1efca62aac8de8b3a3c3f6eb2f1aa1d7d09a26
Bean2005 <christopher.salewski@gmail.com>
2022-11-22 12:21:52 -0500

    manager login

--- login.js
+++ login.js
@@ -14,3 +14,17 @@ function deleteAcc(){
 function gMenu(){
     window.location.replace("./gMenu.html");
 }
+
+function login(id){
+    if(document.getElementById(id).parentElement.children[1].lastElementChild.value.length > 0 && document.getElementById(id).parentElement.children[2].lastElementChild.value.length > 0)
+    {
+        if(document.getElementById(id).parentElement.children[1].lastElementChild.value == "admin" && document.getElementById(id).parentElement.children[2].lastElementChild.value == "1234")
+        {
+            window.location.replace("./gMenu.html");
+        }else{
+            window.location.replace("./menu.html");
+        }
+    }else{
+        alert("Please fill out both fields");
+    }
+}
